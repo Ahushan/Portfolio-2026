736f40f527bde995347bb66de46cc9f947b4ba7f
Albatross <ahushanahushan@gmail.com>
2026-04-14 15:59:02 +0530

    project card  components style have updated

--- src/pages/project/Projects.tsx
+++ src/pages/project/Projects.tsx
@@ -23,7 +23,7 @@ const Projects = () => {
       </h1>
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-20">
         {projectsData.map((project, index) => (
-          <ProjectCard key={index} project={project} index={index} />
+          <ProjectCard key={index} project={project} />
         ))}
       </div>
     </div>
