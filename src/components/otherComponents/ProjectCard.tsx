3344814ee101759be378892bdbb4928d2caadadf
Albatross <ahushanahushan@gmail.com>
2026-04-14 16:02:01 +0530

    Project card  style have updated

--- src/components/otherComponents/ProjectCard.tsx
+++ src/components/otherComponents/ProjectCard.tsx
@@ -50,11 +50,11 @@ const ProjectCard = ({ project }: ProjectCardProps) => {
       <AnimatePresence>
         {isOpen && (
           <motion.div
-            className="fixed inset-0 z-50 flex items-center justify-center bg-black/70 backdrop-blur-sm"
+            className="fixed inset-0 z-50 flex items-center justify-center p-2 bg-black/70 backdrop-blur-sm"
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
             exit={{ opacity: 0 }}
-            onClick={() => setIsOpen(false)} // ✅ outside click
+            onClick={() => setIsOpen(false)}
           >
             {/* MODAL BOX */}
             <motion.div
